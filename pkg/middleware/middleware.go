5bcd6242e69cfe49ac662885b85c138b4df36c59
Kkrusti <carlesming@hotmail.com>
2026-04-04 21:04:06 +0200

    feat: update Strict-Transport-Security header to include preload directive

--- pkg/middleware/middleware.go
+++ pkg/middleware/middleware.go
@@ -41,7 +41,7 @@ func SecurityHeaders(next http.HandlerFunc) http.HandlerFunc {
 		w.Header().Set("X-Frame-Options", "DENY")
 		w.Header().Set("Referrer-Policy", "strict-origin-when-cross-origin")
 		w.Header().Set("Content-Security-Policy", "default-src 'none'")
-		w.Header().Set("Strict-Transport-Security", "max-age=31536000; includeSubDomains")
+		w.Header().Set("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload")
 		next(w, r)
 	}
 }
